0b42b4b0045295055e9dff4e455c8a4f91168cde
CC11001100 <CC11001100@qq.com>
2023-03-11 21:14:16 +0800

    feat: 增加tidb测试脚本

--- scripts/dockers.sh
+++ scripts/dockers.sh
@@ -2,4 +2,3 @@
 
 docker run -itd --name mongo -p 27017:27017 mongo --auth -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=123456
 
-docker run --name tidb-server -d -p 4000:4000 -p 10080:10080 pingcap/tidb:latest
