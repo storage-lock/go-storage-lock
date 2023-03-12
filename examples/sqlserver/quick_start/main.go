0ff4bc3b99458647c0eb70bc3a950e448a8a4691
CC11001100 <CC11001100@qq.com>
2023-03-13 00:03:49 +0800

    feat: 更新SQLServer的示例代码及文档

--- examples/sqlserver/quick_start/main.go
+++ examples/sqlserver/quick_start/main.go
@@ -20,14 +20,14 @@ func main() {
 	// 第一步创建一把分布式锁
 	lock, err := storage_lock.NewSqlServerStorageLock(context.Background(), lockId, dsn)
 	if err != nil {
-		fmt.Printf("Create Lock Failed: %v\n", err.Error())
+		fmt.Printf("[ %s ] Create Lock Failed: %v\n", time.Now().Format("2006-01-02 15:04:05"), err)
 		return
 	}
 
 	// 第二步使用这把锁，这里就模拟多个节点竞争执行的情况，他们会线程安全的往resource里写数据
 	resource := strings.Builder{}
 	var wg sync.WaitGroup
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		workerId := fmt.Sprintf("worker-%d", i)
 		wg.Add(1)
 		go func() {
@@ -36,24 +36,24 @@ func main() {
 			// 获取锁
 			err := lock.Lock(context.Background(), workerId)
 			if err != nil {
-				fmt.Printf("workerId = %s, lock failed: %s \n", workerId, err.Error())
+				fmt.Printf("[ %s ] workerId = %s, lock failed: %v \n", time.Now().Format("2006-01-02 15:04:05"), workerId, err)
 				return
 			}
 			// 退出的时候释放锁
 			defer func() {
 				err := lock.UnLock(context.Background(), workerId)
 				if err != nil {
-					fmt.Printf("workerId = %s, unlock failed: %s \n", workerId, err.Error())
+					fmt.Printf("[ %s ] workerId = %s, unlock failed: %v \n", time.Now().Format("2006-01-02 15:04:05"), workerId, err)
 					return
 				}
 			}()
 
 			// 假装有耗时的操作
-			fmt.Printf("workerId = %s, begin write resource \n", workerId)
+			fmt.Printf("[ %s ] workerId = %s, begin write resource \n", time.Now().Format("2006-01-02 15:04:05"), workerId)
 			time.Sleep(time.Second * 3)
 			// 接下来是操作竞态资源
 			resource.WriteString(workerId)
-			fmt.Printf("workerId = %s, write resource done \n", workerId)
+			fmt.Printf("[ %s ] workerId = %s, write resource done \n", time.Now().Format("2006-01-02 15:04:05"), workerId)
 			resource.WriteString("\n")
 
 		}()
@@ -61,7 +61,40 @@ func main() {
 	wg.Wait()
 
 	// 观察最终的输出是否和日志一致
-	fmt.Println("Resource: ")
+	fmt.Printf("[ %s ] Resource: \n", time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println(resource.String())
+
+	// Output:
+	// [ 2023-03-13 00:00:05 ] workerId = worker-2, begin write resource
+	// [ 2023-03-13 00:00:08 ] workerId = worker-2, write resource done
+	// [ 2023-03-13 00:00:08 ] workerId = worker-8, begin write resource
+	// [ 2023-03-13 00:00:11 ] workerId = worker-8, write resource done
+	// [ 2023-03-13 00:00:11 ] workerId = worker-7, begin write resource
+	// [ 2023-03-13 00:00:14 ] workerId = worker-7, write resource done
+	// [ 2023-03-13 00:00:15 ] workerId = worker-1, begin write resource
+	// [ 2023-03-13 00:00:18 ] workerId = worker-1, write resource done
+	// [ 2023-03-13 00:00:18 ] workerId = worker-3, begin write resource
+	// [ 2023-03-13 00:00:21 ] workerId = worker-3, write resource done
+	// [ 2023-03-13 00:00:23 ] workerId = worker-4, begin write resource
+	// [ 2023-03-13 00:00:26 ] workerId = worker-4, write resource done
+	// [ 2023-03-13 00:00:28 ] workerId = worker-6, begin write resource
+	// [ 2023-03-13 00:00:31 ] workerId = worker-6, write resource done
+	// [ 2023-03-13 00:00:32 ] workerId = worker-0, begin write resource
+	// [ 2023-03-13 00:00:35 ] workerId = worker-0, write resource done
+	// [ 2023-03-13 00:00:35 ] workerId = worker-9, begin write resource
+	// [ 2023-03-13 00:00:38 ] workerId = worker-9, write resource done
+	// [ 2023-03-13 00:00:38 ] workerId = worker-5, begin write resource
+	// [ 2023-03-13 00:00:41 ] workerId = worker-5, write resource done
+	// [ 2023-03-13 00:00:41 ] Resource:
+	// worker-2
+	// worker-8
+	// worker-7
+	// worker-1
+	// worker-3
+	// worker-4
+	// worker-6
+	// worker-0
+	// worker-9
+	// worker-5
 
 }
--- examples/sqlserver/with_detailed_setting/main.go
+++ examples/sqlserver/with_detailed_setting/main.go
@@ -42,7 +42,7 @@ func main() {
 	// 第三步开始使用锁，模拟多个节点竞争同一个锁使用的情况
 	resource := strings.Builder{}
 	var wg sync.WaitGroup
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		workerId := fmt.Sprintf("worker-%d", i)
 		wg.Add(1)
 		go func() {
@@ -51,24 +51,24 @@ func main() {
 			// 获取锁
 			err := lock.Lock(context.Background(), workerId)
 			if err != nil {
-				fmt.Printf("workerId = %s, lock failed: %s \n", workerId, err.Error())
+				fmt.Printf("[ %s ] workerId = %s, lock failed: %v \n", time.Now().Format("2006-01-02 15:04:05"), workerId, err)
 				return
 			}
 			// 退出的时候释放锁
 			defer func() {
 				err := lock.UnLock(context.Background(), workerId)
 				if err != nil {
-					fmt.Printf("workerId = %s, unlock failed: %s \n", workerId, err.Error())
+					fmt.Printf("[ %s ] workerId = %s, unlock failed: %v \n", time.Now().Format("2006-01-02 15:04:05"), workerId, err)
 					return
 				}
 			}()
 
 			// 假装有耗时的操作
-			fmt.Printf("workerId = %s, begin write resource \n", workerId)
+			fmt.Printf("[ %s ] workerId = %s, begin write resource \n", time.Now().Format("2006-01-02 15:04:05"), workerId)
 			time.Sleep(time.Second * 3)
 			// 接下来是操作竞态资源
 			resource.WriteString(workerId)
-			fmt.Printf("workerId = %s, write resource done \n", workerId)
+			fmt.Printf("[ %s ] workerId = %s, write resource done \n", time.Now().Format("2006-01-02 15:04:05"), workerId)
 			resource.WriteString("\n")
 
 		}()
@@ -76,7 +76,40 @@ func main() {
 	wg.Wait()
 
 	// 观察最终的输出是否和日志一致
-	fmt.Println("Resource: ")
+	fmt.Printf("[ %s ] Resource: \n", time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Println(resource.String())
+
+	// Output:
+	// [ 2023-03-13 00:01:32 ] workerId = worker-0, begin write resource
+	// [ 2023-03-13 00:01:35 ] workerId = worker-0, write resource done
+	// [ 2023-03-13 00:01:35 ] workerId = worker-5, begin write resource
+	// [ 2023-03-13 00:01:38 ] workerId = worker-5, write resource done
+	// [ 2023-03-13 00:01:38 ] workerId = worker-1, begin write resource
+	// [ 2023-03-13 00:01:41 ] workerId = worker-1, write resource done
+	// [ 2023-03-13 00:01:42 ] workerId = worker-9, begin write resource
+	// [ 2023-03-13 00:01:45 ] workerId = worker-9, write resource done
+	// [ 2023-03-13 00:01:46 ] workerId = worker-4, begin write resource
+	// [ 2023-03-13 00:01:49 ] workerId = worker-4, write resource done
+	// [ 2023-03-13 00:01:50 ] workerId = worker-3, begin write resource
+	// [ 2023-03-13 00:01:53 ] workerId = worker-3, write resource done
+	// [ 2023-03-13 00:01:55 ] workerId = worker-2, begin write resource
+	// [ 2023-03-13 00:01:58 ] workerId = worker-2, write resource done
+	// [ 2023-03-13 00:01:58 ] workerId = worker-6, begin write resource
+	// [ 2023-03-13 00:02:01 ] workerId = worker-6, write resource done
+	// [ 2023-03-13 00:02:02 ] workerId = worker-7, begin write resource
+	// [ 2023-03-13 00:02:05 ] workerId = worker-7, write resource done
+	// [ 2023-03-13 00:02:06 ] workerId = worker-8, begin write resource
+	// [ 2023-03-13 00:02:09 ] workerId = worker-8, write resource done
+	// [ 2023-03-13 00:02:09 ] Resource:
+	// worker-0
+	// worker-5
+	// worker-1
+	// worker-9
+	// worker-4
+	// worker-3
+	// worker-2
+	// worker-6
+	// worker-7
+	// worker-8
 
 }
